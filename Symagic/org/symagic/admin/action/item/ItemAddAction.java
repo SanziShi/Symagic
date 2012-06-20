09935f58dd215fc7e2da21628f332a1923de7e0a
Jing Hao <hao3039032@163.com>
2012-06-20 22:31:00 +0800

    添加商品详情的Action

--- Symagic/org/symagic/admin/action/item/ItemAddAction.java
+++ Symagic/org/symagic/admin/action/item/ItemAddAction.java
@@ -2,12 +2,15 @@ package org.symagic.admin.action.item;
 
 import java.io.File;
 import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
 
 import javax.servlet.ServletContext;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.struts2.util.ServletContextAware;
 import org.symagic.common.db.bean.BeanBook;
+import org.symagic.common.utility.presentation.bean.TimeBean;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -23,44 +26,99 @@ public class ItemAddAction extends ActionSupport implements ServletContextAware 
 	 */
 	private static final long serialVersionUID = -6680323098777982598L;
 
+	/**
+	 * 
+	 */
 	private String ISBN;
 
+	/**
+	 * 
+	 */
 	private String itemName;
 
+	/**
+	 * 
+	 */
 	private String itemAuthor;
 
+	/**
+	 * 
+	 */
 	private String itemPublisher;
 
-	private String itemPublishingTime;
+	/**
+	 * 
+	 */
+	private TimeBean itemPublishingTime;
 
+	/**
+	 * 
+	 */
 	private Integer itemEdition;
 
+	/**
+	 * 
+	 */
 	private Integer itemPage;
 
+	/**
+	 * 
+	 */
 	private String itemBinding;
 
+	/**
+	 * 
+	 */
 	private Integer itemFormat;
 
-	private Double itemPrice;
+	/**
+	 * 
+	 */
+	private Float itmePublishingPrice;
 
-	private Double itmePublishingPrice;
+	/**
+	 * 
+	 */
+	private Float itemDiscount;
 
-	private Double itemDiscount;
+	/**
+	 * 
+	 */
+	private Integer itemInventory;
 
-	private Integer itemStock;
-
+	/**
+	 * 
+	 */
 	private String itemBookDescribe;
 
+	/**
+	 * 
+	 */
 	private Integer itemBookClassID;
 
+	/**
+	 * 
+	 */
 	private File picture;
 
+	/**
+	 * 
+	 */
 	private String pictureFileName;
 
+	/**
+	 * 
+	 */
 	private boolean formValidateResult;
 
+	/**
+	 * 
+	 */
 	private ServletContext context;
 
+	/**
+	 * 用于存放上传的图片
+	 */
 	private String shopImageFileFolder;
 
 	@Override
@@ -83,13 +141,27 @@ public class ItemAddAction extends ActionSupport implements ServletContextAware 
 
 		FileUtils.moveFile(picture, destFile);
 		
-		//生成数据库所需信息
-		BeanBook book = new BeanBook();
+		//生成时间
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		GregorianCalendar calender = new GregorianCalendar(itemPublishingTime.getYear(), itemPublishingTime.getMonth(), itemPublishingTime.getDay());
 		
+
+		// 生成数据库所需信息
+		BeanBook book = new BeanBook();
+
 		book.setIsbn(ISBN);
 		book.setBookName(itemName);
 		book.setAuthor(itemAuthor);
 		book.setPublisher(itemPublisher);
+		book.setPublisherDate(dateFormat.format(calender.getTime()));
+		book.setVersion(itemEdition);
+		book.setPage(itemPage);
+		book.setBinding(itemBinding);
+		book.setFolio(itemFormat.toString());
+		book.setMarketPrice(itmePublishingPrice);
+//		book.setDiscount(itemDiscount);
+//		
+		
 
 		return SUCCESS;
 	}
@@ -97,14 +169,13 @@ public class ItemAddAction extends ActionSupport implements ServletContextAware 
 	@Override
 	public void validate() {
 
+		//处理商品类别以外其他都不能为空
 		if (ISBN == null || itemName == null || itemAuthor == null
 				|| itemPublisher == null || itemPublishingTime == null
 				|| itemEdition == null || itemPage == null
-				|| itemBinding == null || itemFormat == null
-				|| itemPrice == null || itmePublishingPrice == null
-				|| itemDiscount == null || itemStock == null
-				|| itemBookDescribe == null || itemBookClassID == null
-				|| picture == null) {
+				|| itemBinding == null || itemFormat == null || itmePublishingPrice == null
+				|| itemDiscount == null || itemInventory == null
+				|| itemBookDescribe == null || picture == null) {
 			formValidateResult = false;
 		} else {
 			formValidateResult = true;
@@ -112,153 +183,9 @@ public class ItemAddAction extends ActionSupport implements ServletContextAware 
 
 	}
 
-	public String getISBN() {
-		return ISBN;
-	}
-
-	public void setISBN(String iSBN) {
-		ISBN = iSBN;
-	}
-
-	public String getItemName() {
-		return itemName;
-	}
-
-	public void setItemName(String itemName) {
-		this.itemName = itemName;
-	}
-
-	public String getItemAuthor() {
-		return itemAuthor;
-	}
-
-	public void setItemAuthor(String itemAuthor) {
-		this.itemAuthor = itemAuthor;
-	}
-
-	public String getItemPublisher() {
-		return itemPublisher;
-	}
-
-	public void setItemPublisher(String itemPublisher) {
-		this.itemPublisher = itemPublisher;
-	}
-
-	public String getItemPublishingTime() {
-		return itemPublishingTime;
-	}
-
-	public void setItemPublishingTime(String itemPublishingTime) {
-		this.itemPublishingTime = itemPublishingTime;
-	}
-
-	public Integer getItemEdition() {
-		return itemEdition;
-	}
-
-	public void setItemEdition(Integer itemEdition) {
-		this.itemEdition = itemEdition;
-	}
-
-	public Integer getItemPage() {
-		return itemPage;
-	}
-
-	public void setItemPage(Integer itemPage) {
-		this.itemPage = itemPage;
-	}
-
-	public String getItemBinding() {
-		return itemBinding;
-	}
-
-	public void setItemBinding(String itemBinding) {
-		this.itemBinding = itemBinding;
-	}
-
-	public Integer getItemFormat() {
-		return itemFormat;
-	}
-
-	public void setItemFormat(Integer itemFormat) {
-		this.itemFormat = itemFormat;
-	}
-
-	public Double getItemPrice() {
-		return itemPrice;
-	}
-
-	public void setItemPrice(Double itemPrice) {
-		this.itemPrice = itemPrice;
-	}
-
-	public Double getItmePublishingPrice() {
-		return itmePublishingPrice;
-	}
-
-	public void setItmePublishingPrice(Double itmePublishingPrice) {
-		this.itmePublishingPrice = itmePublishingPrice;
-	}
-
-	public Double getItemDiscount() {
-		return itemDiscount;
-	}
-
-	public void setItemDiscount(Double itemDiscount) {
-		this.itemDiscount = itemDiscount;
-	}
-
-	public Integer getItemStock() {
-		return itemStock;
-	}
-
-	public void setItemStock(Integer itemStock) {
-		this.itemStock = itemStock;
-	}
-
-	public String getItemBookDescribe() {
-		return itemBookDescribe;
-	}
-
-	public void setItemBookDescribe(String itemBookDescribe) {
-		this.itemBookDescribe = itemBookDescribe;
-	}
-
-	public Integer getItemBookClassID() {
-		return itemBookClassID;
-	}
-
-	public void setItemBookClassID(Integer itemBookClassID) {
-		this.itemBookClassID = itemBookClassID;
-	}
-
-	public File getPicture() {
-		return picture;
-	}
-
-	public void setPicture(File picture) {
-		this.picture = picture;
-	}
-
 	@Override
 	public void setServletContext(ServletContext arg0) {
 		context = arg0;
-	}
-
-	public String getPictureFileName() {
-		return pictureFileName;
-	}
-
-	public void setPictureFileName(String pictureFileName) {
-		this.pictureFileName = pictureFileName;
-	}
-
-	public String getShopImageFileFolder() {
-		return shopImageFileFolder;
-	}
-
-	public void setShopImageFileFolder(String shopImageFileFolder) {
-		this.shopImageFileFolder = shopImageFileFolder;
 	}
 
 }
---
+++ Symagic/org/symagic/common/action/item/ItemDetailAction.java
@@ -0,0 +1,353 @@
+package org.symagic.common.action.item;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.symagic.common.db.bean.BeanBook;
+import org.symagic.common.db.func.DaoBook;
+import org.symagic.common.utility.inventory.InventoryUtility;
+import org.symagic.common.utility.presentation.bean.CatalogBean;
+import org.symagic.common.utility.presentation.bean.CommentBean;
+import org.symagic.common.utility.presentation.bean.TimeBean;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ItemDetailAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1237374602453411556L;
+
+	/**
+	 * 输入的itemID
+	 */
+	private Integer itemID;
+
+	/**
+	 * 
+	 */
+	private String ISBN;
+
+	/**
+	 * 
+	 */
+	private String itemName;
+
+	/**
+	 * 
+	 */
+	private String itemAuthor;
+
+	/**
+	 * 
+	 */
+	private String itemPublisher;
+
+	/**
+	 * 
+	 */
+	private String itemPublishingTime;
+
+	/**
+	 * 
+	 */
+	private Integer itemEdition;
+
+	/**
+	 * 
+	 */
+	private Integer itemPage;
+
+	/**
+	 * 
+	 */
+	private String itemBinding;
+
+	/**
+	 * 
+	 */
+	private String itemFormat;
+
+	/**
+	 * 
+	 */
+	private Float itemPublishingPrice;
+	
+	private Float itemPrice;
+
+	/**
+	 * 
+	 */
+	private Float itemDiscount;
+
+	/**
+	 * 
+	 */
+	private Boolean itemInventory;
+
+	/**
+	 * 
+	 */
+	private String itemBookDescribe;
+
+	/**
+	 * 
+	 */
+	private List<CatalogBean> itemBookClass;
+
+	/**
+	 * 
+	 */
+	private Integer itemComprehensiveRate;
+
+	/**
+	 * 
+	 */
+	private List<CommentBean> commentList;
+
+	private Integer page;
+
+	private Integer totalPage;
+	private Integer currentPage;
+
+	/**
+	 * 
+	 */
+	private List<CatalogBean> catalog;
+
+	private DaoBook daoBook;
+
+	@Override
+	public String execute() throws Exception {
+
+		BeanBook book = daoBook.getDeatil(itemID);
+
+		ISBN = book.getIsbn();
+		itemName = book.getBookName();
+		itemAuthor = book.getAuthor();
+
+		itemPublisher = book.getPublisher();
+
+		itemPublishingTime = book.getPublisherDate();
+
+		itemEdition = book.getVersion();
+
+		itemPage = book.getPage();
+
+		itemBinding = book.getBinding();
+
+		itemFormat = book.getFolio();
+
+		itemPublishingPrice = book.getMarketPrice();
+
+		itemDiscount = book.getDiscount();
+
+		itemInventory = InventoryUtility.checkInventory(book.getInventory());
+
+		itemBookDescribe = book.getBookDesc();
+
+		itemPrice = itemPublishingPrice * itemDiscount;
+		
+//		book.get
+//		
+//		/**
+//		 * 
+//		 */
+//		private List<CatalogBean> itemBookClass;
+//
+//		/**
+//		 * 
+//		 */
+//		private Integer itemComprehensiveRate;
+//
+//		/**
+//		 * 
+//		 */
+//		private List<CommentBean> commentList;
+//
+//		/**
+//		 * 
+//		 */
+//		private List<CatalogBean> catalog;
+//
+//		private Integer page;
+//
+//		private Integer totalPage;
+//		private Integer currentPage;
+
+		return SUCCESS;
+	}
+
+	public Integer getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(Integer itemID) {
+		this.itemID = itemID;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public String getItemAuthor() {
+		return itemAuthor;
+	}
+
+	public void setItemAuthor(String itemAuthor) {
+		this.itemAuthor = itemAuthor;
+	}
+
+	public String getItemPublisher() {
+		return itemPublisher;
+	}
+
+	public void setItemPublisher(String itemPublisher) {
+		this.itemPublisher = itemPublisher;
+	}
+
+	public String getItemPublishingTime() {
+		return itemPublishingTime;
+	}
+
+	public void setItemPublishingTime(String itemPublishingTime) {
+		this.itemPublishingTime = itemPublishingTime;
+	}
+
+	public Integer getItemEdition() {
+		return itemEdition;
+	}
+
+	public void setItemEdition(Integer itemEdition) {
+		this.itemEdition = itemEdition;
+	}
+
+	public Integer getItemPage() {
+		return itemPage;
+	}
+
+	public void setItemPage(Integer itemPage) {
+		this.itemPage = itemPage;
+	}
+
+	public String getItemBinding() {
+		return itemBinding;
+	}
+
+	public void setItemBinding(String itemBinding) {
+		this.itemBinding = itemBinding;
+	}
+
+	public String getItemFormat() {
+		return itemFormat;
+	}
+
+	public void setItemFormat(String itemFormat) {
+		this.itemFormat = itemFormat;
+	}
+
+	public Float getItemDiscount() {
+		return itemDiscount;
+	}
+
+	public void setItemDiscount(Float itemDiscount) {
+		this.itemDiscount = itemDiscount;
+	}
+
+	public String getItemBookDescribe() {
+		return itemBookDescribe;
+	}
+
+	public void setItemBookDescribe(String itemBookDescribe) {
+		this.itemBookDescribe = itemBookDescribe;
+	}
+
+	public List<CatalogBean> getItemBookClass() {
+		return itemBookClass;
+	}
+
+	public void setItemBookClass(List<CatalogBean> itemBookClass) {
+		this.itemBookClass = itemBookClass;
+	}
+
+	public Integer getItemComprehensiveRate() {
+		return itemComprehensiveRate;
+	}
+
+	public void setItemComprehensiveRate(Integer itemComprehensiveRate) {
+		this.itemComprehensiveRate = itemComprehensiveRate;
+	}
+
+	public List<CommentBean> getCommentList() {
+		return commentList;
+	}
+
+	public void setCommentList(List<CommentBean> commentList) {
+		this.commentList = commentList;
+	}
+
+	public List<CatalogBean> getCatalog() {
+		return catalog;
+	}
+
+	public void setCatalog(List<CatalogBean> catalog) {
+		this.catalog = catalog;
+	}
+
+	public Integer getPage() {
+		return page;
+	}
+
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+
+	public Integer getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(Integer totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public Integer getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(Integer currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public Boolean getItemInventory() {
+		return itemInventory;
+	}
+
+	public void setItemInventory(Boolean itemInventory) {
+		this.itemInventory = itemInventory;
+	}
+
+	public DaoBook getDaoBook() {
+		return daoBook;
+	}
+
+	public void setDaoBook(DaoBook daoBook) {
+		this.daoBook = daoBook;
+	}
+
+}
