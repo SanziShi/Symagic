ef6bfe0809fdae9c3c2059dc0d5c88f9f5acf7d0
Jing Hao <hao3039032@163.com>
2012-06-22 17:37:23 +0800

    添加OrderService(未完成)

--- Symagic/org/symagic/common/service/OrderService.java
+++ Symagic/org/symagic/common/service/OrderService.java
@@ -1,5 +1,16 @@
 package org.symagic.common.service;
 
-public class OrderService {
+import org.symagic.common.db.func.DaoOrder;
+import org.symagic.common.db.func.OrderRequire;
+import org.symagic.common.utilty.presentation.bean.TimeBean;
 
+public class OrderService {
+	
+	private DaoOrder daoOrder;
+	
+	public void orderList(TimeBean startTime,TimeBean endTime, Integer orderState, Integer page) {
+		
+		OrderRequire require = new OrderRequire();
+		
+	}
 }
