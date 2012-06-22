2a8009b905c0d24eac948d158ea480ba319e2ad2
wanran <shan0xiao0xi@163.com>
2012-06-22 21:50:33 +0800

    添加创建和插入脚本，完成管理员index页面需要接口原型

---
+++ Symagic/org/symagic/common/db/func/DaoLevel.java
@@ -0,0 +1,32 @@
+package org.symagic.common.db.func;
+
+import org.symagic.common.db.bean.BeanLevel;
+
+/**
+ * 封装score_level表相关操作的类
+ * @author wanran
+ *
+ */
+public class DaoLevel {
+	/**
+	 * 向score_level添加一条记录
+	 * @param level	封装score_level表的所有字段的BeanLevel实例
+	 * @return	true 添加成功	false 添加失败
+	 */
+	public boolean add(BeanLevel level)
+	{
+		return false;
+	}
+	
+	/**
+	 * 更新score_level表存在的一条记录
+	 * @param level	封装score_level表的所有字段的BeanLevel实例
+	 * @return true 更新成功	false 更新失败
+	 */
+	public boolean update(BeanLevel level)
+	{
+		return false;
+	}
+	
+	public 
+}
---
+++ doc/insertData.sql
@@ -0,0 +1,3 @@
+/* 管理员表数据插入*/
+
+insert into admin (adminname, password) values ("管理员", md5(12));
