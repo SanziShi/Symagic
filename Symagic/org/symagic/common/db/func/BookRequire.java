c9903aa076ac9e73f951e33796f2930b0c49a298
wanran <shan0xiao0xi@163.com>
2012-06-22 17:53:01 +0800

    完善BookRequire和OrderRequire

--- Symagic/org/symagic/common/db/func/BookRequire.java
+++ Symagic/org/symagic/common/db/func/BookRequire.java
@@ -9,17 +9,33 @@ public class BookRequire {
 	// 内容待定
 	
 // 
-//	itemName:
-//	publisher:
-//	所属目录ID
+	private String itemName = null;
+	private String publisher = null;
+	private String atalogID = null;
 //	year(年);
+	private String year	= null;
 //	version;
+	private Integer version = null;
 //	up_searchPage(页数);
+	private Integer upPage	= null;
 //	low_searchPage;
+	private Integer lowPage	= null;
 //	binding;
+	/**
+	 * 书籍装帧 
+	 * 选择（"精装"或"平装"）
+	 */
+	private String binding	= null;
 //	booksize;
+	private String folio	= null;
 //	up_price;
+	private Float upPrice	= null;
 //	low_price;
+	private Float lowPrice	= null;
 //	discount(discount(0:0`10,1:10`30,2:30`50,3:50`100,4:>100））;
+	private Integer discount	= null;
 //	page（第几页)
+	private Integer page	= null;
+	
+	private Integer lines	= null;
 }
--- Symagic/org/symagic/common/db/func/DaoCatalog.java
+++ Symagic/org/symagic/common/db/func/DaoCatalog.java
@@ -1,9 +1,15 @@
 package org.symagic.common.db.func;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.symagic.common.db.bean.BeanCatalog;
+import org.symagic.common.db.pool.ConnectionPool;
 
 /**
  * 封装书籍分类相关操作的类
@@ -11,6 +17,11 @@ import org.symagic.common.db.bean.BeanCatalog;
  *
  */
 public class DaoCatalog {
+	private Connection conn;
+	private PreparedStatement ps;
+	private Statement st;
+	private ResultSet rs;
+	private int count = 0;
 	
 	/**
 	 * 获取所有书籍分类
@@ -18,6 +29,49 @@ public class DaoCatalog {
 	 */
 	public List<BeanCatalog> getCatalog()
 	{
+		try {
+			conn	= ConnectionPool.getInstance().getConnection();
+			ps	= conn.prepareStatement("");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return new ArrayList<BeanCatalog>();
+	}
+	
+	/**
+	 * 添加书籍分类
+	 * @param catalog 封装着分类信息的BeanCatalog对象
+	 * @return	true 添加成功	false 添加失败
+	 */
+	public boolean addCatalog(BeanCatalog catalog)
+	{
+		try {
+			conn	= ConnectionPool.getInstance().getConnection();
+			ps	= conn.prepareStatement("insert into book_catalog " +
+					"(catalogname, level" +
+					" upid, )");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * 修改书籍分类
+	 * @param catalog 封装书籍分类信息的BeanCatalog对象
+	 * @return	true 修改成功	false 修改失败
+	 */
+	public  boolean modifyCatalog(BeanCatalog catalog)
+	{
+		return false;
 	}
 }
