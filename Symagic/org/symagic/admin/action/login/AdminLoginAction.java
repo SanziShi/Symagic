25e60c809038cbbeeca0859ff29d1c75c4aa99f2
Jing Hao <hao3039032@163.com>
2012-06-22 23:09:16 +0800

    添加错误处理

--- Symagic/org/symagic/admin/action/login/AdminLoginAction.java
+++ Symagic/org/symagic/admin/action/login/AdminLoginAction.java
@@ -12,65 +12,69 @@ public class AdminLoginAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = -9192677626180527058L;
-	
+
 	/**
 	 * 
 	 */
 	private String userName;
-	
+
 	/**
 	 * 
 	 */
 	private String password;
-	
+
 	/**
 	 * 
 	 */
 	private String captchaValue;
-	
+
 	/**
 	 * 
 	 */
 	private String toURL;
-	
 
 	/**
 	 * 
 	 */
 	private Captcha symagicCaptcha;
-	
+
 	private boolean validateResult;
-	
+
 	private DaoAdmin daoAdmin;
+
+	// 错误处理
+	private String errorHeader;
+
+	private String errorSpecification;
 
 	@Override
 	public void validate() {
-		
+
 		validateResult = true;
-		
-		if( userName == null || password == null || captchaValue == null || password.length() > 20 )
+
+		if (userName == null || password == null || captchaValue == null
+				|| password.length() > 20)
 			validateResult = false;
-		
+
 	}
 
 	@Override
 	public String execute() throws Exception {
-		
-		if( !validateResult )
-			return ERROR;
-		
-		if( !symagicCaptcha.validateCaptcha(AdminSessionUtilty.getSessionID(), captchaValue) )
-			return ERROR;
-		
-		if( !daoAdmin.validateAdmin(userName, password) ){
+
+		if (!validateResult
+				|| !symagicCaptcha.validateCaptcha(
+						AdminSessionUtilty.getSessionID(), captchaValue)
+				|| !daoAdmin.validateAdmin(userName, password)) {
+			errorHeader = new String("登录失败");
+			errorSpecification = new String("您输入的用户名或密码有误，请返回检查。");
 			return ERROR;
 		}
-		
+
 		AdminSessionUtilty.logLogin(userName);
-		
+
 		return SUCCESS;
 	}
-	
+
 	public String getUserName() {
 		return userName;
 	}
@@ -125,6 +129,22 @@ public class AdminLoginAction extends ActionSupport {
 
 	public void setDaoAdmin(DaoAdmin daoAdmin) {
 		this.daoAdmin = daoAdmin;
+	}
+
+	public String getErrorHeader() {
+		return errorHeader;
+	}
+
+	public void setErrorHeader(String errorHeader) {
+		this.errorHeader = errorHeader;
+	}
+
+	public String getErrorSpecification() {
+		return errorSpecification;
+	}
+
+	public void setErrorSpecification(String errorSpecification) {
+		this.errorSpecification = errorSpecification;
 	}
 
 }
