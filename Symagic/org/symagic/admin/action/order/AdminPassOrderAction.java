1df95eb28a5118f839cb81097b1fe95cb3bb599d
Jing Hao <hao3039032@163.com>
2012-06-23 17:16:23 +0800

    修改验证码

---
+++ Symagic/org/symagic/admin/action/order/AdminPassOrderAction.java
@@ -0,0 +1,24 @@
+package org.symagic.admin.action.order;
+
+import net.sf.json.util.JSONUtils;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AdminPassOrderAction extends ActionSupport {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2766337819712072649L;
+	private String orderIDList;
+	
+	@Override
+	public String execute() throws Exception {
+		
+		
+		return super.execute();
+	}
+	
+	
+	
+}
