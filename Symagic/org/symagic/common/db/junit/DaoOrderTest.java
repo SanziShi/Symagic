49a6210a82e281f31ad4bd302c0d63fdd885ea20
wanran <shan0xiao0xi@163.com>
2012-06-25 14:18:27 +0800

    修改DaoOrderDetail和BeanOrder, 添加OrderDetail到BeanOrder

--- Symagic/org/symagic/common/db/junit/DaoOrderTest.java
+++ Symagic/org/symagic/common/db/junit/DaoOrderTest.java
@@ -1,69 +1,112 @@
 package org.symagic.common.db.junit;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
 
 import org.junit.Test;
+import org.symagic.common.db.bean.BeanOrder;
+import org.symagic.common.db.bean.BeanOrderDetail;
+import org.symagic.common.db.func.DaoOrder;
 
-public class DaoOrderTest {
+public class DaoOrderTest extends TestCase{
+//
+//	@Test
+//	public void testSearch() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetOrderDetail() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetRowNumber() {
+//		fail("Not yet implemented");
+//	}
 
-	@Test
-	public void testSearch() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testAddOrder() {
+//		DaoOrder dorder	= new DaoOrder();
+//		BeanOrder bo	= new BeanOrder();
+//		BeanOrderDetail detail	= new BeanOrderDetail();
+//		detail.setAmount(10);
+//		detail.setBookId(1);
+//		detail.setBookName("我的第一本书");
+//		detail.setDiscount(0.75f);
+//		detail.setIsbn("1234");
+//		detail.setMarketPrice(100.0f);
+//		
+//		bo.setOrderDate("2012-6-25 10:35:55");
+//		bo.setAddrDetail("北京市");
+//		bo.setDeliveryWay("0");
+//		bo.setMobilenum("186");
+//		bo.setOrderState("0");
+//		bo.setPayment("0");
+//		bo.setPhonenum("1270");
+//		bo.setReceiverName("单小熙");
+//		bo.setUsername("单小熙1");
+//		bo.setZipcode("101300");
+//		bo.setTotalprice(75.0f);
+//		List<BeanOrderDetail> list	= new ArrayList<BeanOrderDetail>();
+//		list.add(detail);
+//		bo.setList(list);
+//		assertEquals(true, dorder.addOrder(bo));
+//	}
 
-	@Test
-	public void testGetOrderDetail() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetRowNumber() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddOrder() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdateOrder() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDeleteOrder() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTotalSaleAmount() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTotalSalesRevenue() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTotalOrderAmount() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetUnauditedOrderAmount() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetFinishOrderAmount() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetLatestOrders() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void testUpdateOrder() {
+//		DaoOrder dorder	= new DaoOrder();
+//		BeanOrder bo	= new BeanOrder();
+//		bo.setOrderId(2);
+//		bo.setAddrDetail("北京市");
+//		bo.setDeliveryWay("0");
+//		bo.setMobilenum("1370");
+//		bo.setOrderState("0");
+//		bo.setPayment("0");
+//		bo.setPhonenum("1270");
+//		bo.setReceiverName("单小熙");
+//		bo.setUsername("单小熙1");
+//		bo.setZipcode("101300");
+//		assertEquals(true, dorder.updateOrder(bo));
+//	}
+//
+//	@Test
+//	public void testDeleteOrder() {
+//		DaoOrder dOrder	= new DaoOrder();
+//		assertEquals(true, dOrder.deleteOrder("单小熙1", 1));
+//	}
+//
+//	@Test
+//	public void testGetTotalSaleAmount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetTotalSalesRevenue() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetTotalOrderAmount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetUnauditedOrderAmount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetFinishOrderAmount() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetLatestOrders() {
+//		fail("Not yet implemented");
+//	}
 
 }
