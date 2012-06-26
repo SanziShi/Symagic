fd33613e73c80ac477579ca68b03a5aea5ec6719
Santa DG <Santa DG@SantaDG-PC>
2012-06-26 11:09:06 +0800

    后台搜索和商品列表

--- webapp/admin/js/gz.js
+++ webapp/admin/js/gz.js
@@ -8,3 +8,25 @@ function delete_tag(id){
 	var tag = document.getElementById(id);
 	tag.parentNode.parentNode.removeChild(tag);
 	}
+	
+function show_item_search(e)
+{
+	if(e.className=='collapse')
+	{
+		e.className='collapsed';
+		$('#item_search1').slideDown(70);
+	}
+	else
+	{
+		e.className='collapse';
+		$('#item_search1').slideUp(70);
+	}
+}
+
+function expanse(e)
+{
+	
+		e.className='collapsed';
+		$('#item_search1').slideDown(70);
+	
+}
