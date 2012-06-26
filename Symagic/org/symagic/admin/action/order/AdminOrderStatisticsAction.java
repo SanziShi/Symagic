5475f826ac113a9c766ec38454bc4eaf210105ce
Jing Hao <hao3039032@163.com>
2012-06-27 00:05:05 +0800

    完成管理员删除订单

---
+++ Symagic/org/symagic/admin/action/order/AdminOrderStatisticsAction.java
@@ -0,0 +1,12 @@
+package org.symagic.admin.action.order;
+
+import org.symagic.common.action.catalog.CatalogBase;
+
+public class AdminOrderStatisticsAction extends CatalogBase {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7238512637976603546L;
+
+}
