f00efb4caee8bffa5d7db08a1bc76838632925a6
Jing Hao <hao3039032@163.com>
2012-06-27 20:24:27 +0800

    管理员拦截器添加非法URL访问重置

--- Symagic/org/symagic/common/interceptor/AdminLoginInterceptor.java
+++ Symagic/org/symagic/common/interceptor/AdminLoginInterceptor.java
@@ -1,5 +1,7 @@
 package org.symagic.common.interceptor;
 
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.struts2.StrutsStatics;
@@ -10,7 +12,8 @@ import com.opensymphony.xwork2.interceptor.MethodFilterInterceptor;
 
 /**
  * 管理员操作的login拦截器
- * @author hao 
+ * 
+ * @author hao
  */
 public class AdminLoginInterceptor extends MethodFilterInterceptor {
 
@@ -18,7 +21,12 @@ public class AdminLoginInterceptor extends MethodFilterInterceptor {
 	 * 
 	 */
 	private static final long serialVersionUID = 2638872275691843170L;
-	
+
+	/**
+	 * 被认为是非法请求的URL集合
+	 */
+	private Set<String> guestIllegalURL;
+
 	/**
 	 * 拦截未登录管理员账户的客户端发来的请求
 	 */
@@ -36,15 +44,33 @@ public class AdminLoginInterceptor extends MethodFilterInterceptor {
 				.getInvocationContext().get(StrutsStatics.HTTP_REQUEST);
 		StringBuffer preURL = request.getRequestURL();
 
+		String contextPath = request.getContextPath();
+		int illegalCheckStartIndex = preURL.indexOf(contextPath);
+		String illegalCheckPath = preURL.substring(illegalCheckStartIndex + 1);
+
 		// 设置url
-		invocation
-				.getInvocationContext()
-				.getValueStack()
-				.getContext()
-				.put("toURL",
-						preURL.substring(preURL.toString().lastIndexOf('/') + 1));
+		if (guestIllegalURL.contains(illegalCheckPath)) {
+			invocation.getInvocationContext().getValueStack().getContext()
+					.put("toURL", "index");
+		} else {
+
+			invocation
+					.getInvocationContext()
+					.getValueStack()
+					.getContext()
+					.put("toURL",
+							preURL.substring(preURL.toString().lastIndexOf('/') + 1));
+		}
 
 		return "enforceLogin";
+	}
+
+	public Set<String> getGuestIllegalURL() {
+		return guestIllegalURL;
+	}
+
+	public void setGuestIllegalURL(Set<String> guestIllegalURL) {
+		this.guestIllegalURL = guestIllegalURL;
 	}
 
 }
