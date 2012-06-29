56ca1072eb9d2a15609effac5e7d5bf853504147
Jing Hao <hao3039032@163.com>
2012-06-29 19:13:24 +0800

    修改验证吗（仿老外的）

--- Symagic/org/symagic/common/utilty/captcha/GMailCaptchaEngine.java
+++ Symagic/org/symagic/common/utilty/captcha/GMailCaptchaEngine.java
@@ -5,58 +5,66 @@ import java.awt.Font;
 import java.awt.image.ImageFilter;
 
 import com.octo.captcha.component.image.backgroundgenerator.BackgroundGenerator;
-import com.octo.captcha.component.image.backgroundgenerator.FunkyBackgroundGenerator;
-import com.octo.captcha.component.image.color.RandomListColorGenerator;
-import com.octo.captcha.component.image.deformation.ImageDeformation;
+import com.octo.captcha.component.image.backgroundgenerator.UniColorBackgroundGenerator;
+import com.octo.captcha.component.image.color.RandomRangeColorGenerator;
 import com.octo.captcha.component.image.deformation.ImageDeformationByFilters;
 import com.octo.captcha.component.image.fontgenerator.FontGenerator;
 import com.octo.captcha.component.image.fontgenerator.RandomFontGenerator;
 import com.octo.captcha.component.image.textpaster.DecoratedRandomTextPaster;
 import com.octo.captcha.component.image.textpaster.TextPaster;
+import com.octo.captcha.component.image.textpaster.textdecorator.BaffleTextDecorator;
 import com.octo.captcha.component.image.textpaster.textdecorator.TextDecorator;
 import com.octo.captcha.component.image.wordtoimage.DeformedComposedWordToImage;
 import com.octo.captcha.component.image.wordtoimage.WordToImage;
-import com.octo.captcha.component.word.FileDictionary;
-import com.octo.captcha.component.word.wordgenerator.ComposeDictionaryWordGenerator;
+import com.octo.captcha.component.word.wordgenerator.RandomWordGenerator;
 import com.octo.captcha.component.word.wordgenerator.WordGenerator;
 import com.octo.captcha.engine.image.ListImageCaptchaEngine;
 import com.octo.captcha.image.gimpy.GimpyFactory;
 
 @SuppressWarnings("deprecation")
 public class GMailCaptchaEngine extends ListImageCaptchaEngine {
+
+	public static final Integer WORD_MIN_LENGTH = new Integer(4);
+	public static final Integer WORD_MAX_LENGTH = new Integer(6);
+
+	public static final Integer IMAGE_WIDTH = new Integer(125);
+	public static final Integer IMAGE_HEIGHT = new Integer(30);
+
+	public static final Integer FONT_MIN_SIZE = new Integer(15);
+	public static final Integer FONT_MAX_SIZE = new Integer(20);
+
 	@Override
 	protected void buildInitialFactories() {
-		int minWordLength = 4;
-		int maxWordLength = 5;
-		int fontSize = 25;
-		int imageWidth = 125;
-		int imageHeight = 50;
 
-		// word generator
-		WordGenerator dictionnaryWords = new ComposeDictionaryWordGenerator(
-				new FileDictionary("toddlist"));
+		WordGenerator wordGenerator = (new RandomWordGenerator(
+				"23456789ABCDEFGHJKMNPQRSTUVWXY"));
 
-		// word2image components
-		TextPaster randomPaster = new DecoratedRandomTextPaster(minWordLength,
-				maxWordLength, new RandomListColorGenerator(new Color[] {
-						new Color(23, 10, 27), new Color(220, 5, 11),
-						new Color(23, 67, 172) }), new TextDecorator[] {});
-		BackgroundGenerator background = new FunkyBackgroundGenerator(
-				imageWidth, imageHeight);
-		FontGenerator font = new RandomFontGenerator(fontSize, fontSize,
-				new Font[] { new Font("nyala", Font.PLAIN, fontSize),
-						new Font("Bell MT", Font.PLAIN, fontSize),
-						new Font("Credit valley", Font.ITALIC, fontSize) });
+		BackgroundGenerator backgroundGenerator = new UniColorBackgroundGenerator(
+				IMAGE_WIDTH, IMAGE_HEIGHT);
 
-		ImageDeformation postDef = new ImageDeformationByFilters(
-				new ImageFilter[] {});
-		ImageDeformation backDef = new ImageDeformationByFilters(
-				new ImageFilter[] {});
-		ImageDeformation textDef = new ImageDeformationByFilters(
-				new ImageFilter[] {});
+		Font[] fontsList = new Font[] { Font.decode("Arial"),
+				Font.decode("Georgia"), Font.decode("Verdana"),
+				Font.decode("Courier New") };
+		FontGenerator fontGenerator = new RandomFontGenerator(FONT_MIN_SIZE,
+				FONT_MAX_SIZE, fontsList);
 
-		WordToImage word2image = new DeformedComposedWordToImage(font,
-				background, randomPaster, backDef, textDef, postDef);
-		addFactory(new GimpyFactory(dictionnaryWords, word2image));
+		RandomRangeColorGenerator cgen = new RandomRangeColorGenerator(
+				new int[] { 0, 128 }, new int[] { 0, 128 },
+				new int[] { 0, 196 });
+
+		TextDecorator[] textdecorators = new TextDecorator[] { new BaffleTextDecorator(
+				new Integer(1), Color.WHITE) };
+
+		TextPaster textPaster = new DecoratedRandomTextPaster(WORD_MIN_LENGTH,
+				WORD_MAX_LENGTH, cgen, textdecorators);
+
+		WordToImage wordToImage = new DeformedComposedWordToImage(
+				fontGenerator, backgroundGenerator, textPaster,
+				new ImageDeformationByFilters(new ImageFilter[] {}),
+				new ImageDeformationByFilters(new ImageFilter[] {}),
+				new ImageDeformationByFilters(new ImageFilter[] {}));
+
+		addFactory(new GimpyFactory(wordGenerator, wordToImage));
 	}
+
 }
