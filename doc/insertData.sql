e0b5babced51ce65fe4002822a18fee7a1092e93
Sanzi <Sanzi@125.216.227.22>
2012-06-29 11:55:21 +0800

    数据库修改

--- doc/insertData.sql
+++ doc/insertData.sql
@@ -670,13 +670,13 @@ insert into order_detail(ORDERID,BOOKID,ISBN,BOOKNAME,MARKETPRICE,DISCOUNT,AMOUN
 -- 插入favority_folder
 insert into favority_folder(username,bookID)
       values ("641567179@qq.com",1);
-insert into favority_folder(username)
+insert into favority_folder(username,bookID)
       values ("641567179@qq.com",2);
-insert into favority_folder(username)
+insert into favority_folder(username,bookID)
       values ("641567179@qq.com",12);
-insert into favority_folder(username)
+insert into favority_folder(username,bookID)
       values ("641567179@qq.com",23);
-insert into favority_folder(username)
+insert into favority_folder(username,bookID)
       values ("641567179@qq.com",30);
       
 
