628d3e4666474818992ff9999f43a08ad9e46143
Jing Hao <hao3039032@163.com>
2012-06-30 21:11:14 +0800

    修正ItemDelete

--- Symagic/org/symagic/admin/action/item/ItemDeleteAction.java
+++ Symagic/org/symagic/admin/action/item/ItemDeleteAction.java
@@ -1,32 +1,63 @@
 package org.symagic.admin.action.item;
 
+import java.io.File;
+
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.util.ServletContextAware;
+import org.symagic.common.db.bean.BeanBook;
 import org.symagic.common.db.func.DaoBook;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ItemDeleteAction extends ActionSupport {
-	
+public class ItemDeleteAction extends ActionSupport implements
+		ServletContextAware {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4066697997147063158L;
-	
-	private Integer itemID;//商品ID
+
+	private Integer itemID;// 商品ID
 	private Boolean deleteResult;
-	
+
 	private DaoBook daoBook;
+
+	/**
+	 * 用于存放上传的图片
+	 */
+	private String shopImageFileFolder;
+
+	/**
+	 * 
+	 */
+	private ServletContext context;
 
 	@Override
 	public String execute() throws Exception {
-		
+
 		deleteResult = false;
-		
-		if( itemID != null ){
-			if( !daoBook.deleteBook(itemID) )
+
+		if (itemID != null) {
+			BeanBook book = daoBook.getDetail(itemID);
+			if (book == null)
 				return super.execute();
-			deleteResult = true;
+
+			String fileFolder = context.getRealPath("/" + shopImageFileFolder);
+			File destFile = new File(fileFolder, book.getPicture().substring(
+					book.getPicture().lastIndexOf('/') + 1));
+
+			if (destFile.exists()) {
+				FileUtils.forceDelete(destFile);
+
+				if (!daoBook.deleteBook(itemID))
+					return super.execute();
+				deleteResult = true;
+			}
+
 		}
-		
+
 		return super.execute();
 	}
 
@@ -53,7 +84,19 @@ public class ItemDeleteAction extends ActionSupport {
 	public void setDaoBook(DaoBook daoBook) {
 		this.daoBook = daoBook;
 	}
-	
-	
-	
+
+	@Override
+	public void setServletContext(ServletContext arg0) {
+		// TODO Auto-generated method stub
+		context = arg0;
+	}
+
+	public String getShopImageFileFolder() {
+		return shopImageFileFolder;
+	}
+
+	public void setShopImageFileFolder(String shopImageFileFolder) {
+		this.shopImageFileFolder = shopImageFileFolder;
+	}
+
 }
