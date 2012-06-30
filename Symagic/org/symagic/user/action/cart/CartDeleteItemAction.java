9915934c16c16fb210f71ff40a01c215ef505b61
zsc <zsc@zsc-PC>
2012-06-30 18:34:13 +0800

    修改

--- Symagic/org/symagic/user/action/cart/CartDeleteItemAction.java
+++ Symagic/org/symagic/user/action/cart/CartDeleteItemAction.java
@@ -1,8 +1,12 @@
 package org.symagic.user.action.cart;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.symagic.common.db.func.DaoCart;
 import org.symagic.user.utilty.UserSessionUtilty;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class CartDeleteItemAction extends ActionSupport {
@@ -10,15 +14,12 @@ public class CartDeleteItemAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 531344796388813069L;
-	private DaoCart daoCart;
-	public DaoCart getDaoCart() {
-		return daoCart;
-	}
-
-	public void setDaoCart(DaoCart daoCart) {
-		this.daoCart = daoCart;
-	}
+	//传入
 	private Integer itemID;
+	//配置项
+	private DaoCart daoCart;
+	//传出
+	
 private boolean deleteResult=false;;
 	@Override
 	public String execute() throws Exception {
@@ -28,6 +29,7 @@ private boolean deleteResult=false;;
 			return SUCCESS;
 		}
 		//购物车没有商品
+		
 		if(UserSessionUtilty.getCart().get(itemID)==null){
 			deleteResult=false;
 			return "success";
@@ -42,17 +44,30 @@ private boolean deleteResult=false;;
 		return super.execute();
 	}
 
-public Integer getItemId() {
-	return itemID;
-}
-public void setItemId(Integer itemId) {
-	this.itemID= itemId;
-}
+
 public boolean isDeleteResult() {
 	return deleteResult;
 }
 public void setDeleteResult(boolean deleteResult) {
 	this.deleteResult = deleteResult;
+}
+
+public DaoCart getDaoCart() {
+	return daoCart;
+}
+
+public void setDaoCart(DaoCart daoCart) {
+	this.daoCart = daoCart;
+}
+
+
+public Integer getItemID() {
+	return itemID;
+}
+
+
+public void setItemID(Integer itemID) {
+	this.itemID = itemID;
 }
 
 
