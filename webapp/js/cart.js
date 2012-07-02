58c1b428ab9d01914bc39965b62a77f576495545
yf <317012583@qq.com>
2012-07-02 11:42:31 +0800

    修复bug

--- webapp/js/cart.js
+++ webapp/js/cart.js
@@ -19,11 +19,36 @@ function checkbox_change(e)
 {
 	alert(e.id);
 }
-/*****从cart页面删除商品*******/
+/*****从cart页面删除商品
 function delete_from_page(id)
 {
 	Ajax({
-		url:'cart/delete?itemID='+id,
+		url:'cart/delete?items='+id,
 		onSuccess:function(e){var a=JSON.parse(e);if(a.deleteResult)location.reload()}
 		})
 }
+*******/
+function delete_form_cart(f)
+{
+	var form=document.getElementById(f);
+	var k=form.getElementsByTagName('input');
+	var l=k.length;
+	for(var i=0;i<l;++i)
+	{
+		alert(k[i].id);
+	}
+	return false;
+	Ajax({
+		url:'cart/delete',
+		data:d,
+		onSuccess:function(e)
+			{
+				var a=JSON.parse(e);
+				if(a.deleteResult)alert('删除成功~');
+			}
+		})
+}
+
+
+
+
