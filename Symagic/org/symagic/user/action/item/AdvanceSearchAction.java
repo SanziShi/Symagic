4af372fa3dd949146cdac28778f5c899777a5b25
zsc <zsc@125.216.227.26>
2012-07-03 11:38:16 +0800

    zsc

--- Symagic/org/symagic/user/action/item/AdvanceSearchAction.java
+++ Symagic/org/symagic/user/action/item/AdvanceSearchAction.java
@@ -150,11 +150,11 @@ public class AdvanceSearchAction extends CatalogBase {
 			return;
 		switch (index) {
 		case 1:
-			require.setFolio("32");
+			require.setFolio("32");break;
 		case 2:
-			require.setFolio("16");
+			require.setFolio("16");break;
 		case 3:
-			require.setFolio("8");
+			require.setFolio("8");break;
 		}
 	}
 
