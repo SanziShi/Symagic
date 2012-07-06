399e3394b8b9555f5500e0779114ebb1a9cb1cb6
hao <Administrator@sse171>
2012-07-06 15:40:21 +0800

    修正地区数据刷新

--- webapp/admin/js/gz.js
+++ webapp/admin/js/gz.js
@@ -901,31 +901,47 @@ function ajax_batch_off(form) {
 }
 
 // 地址选择下拉相关
-function get_district(d, path) {
-	var va = d.value;
-	if (va == 's1') {
-		document.getElementById('level2ID').innerHTML = '<option value="s2">请选择</option>';
-		document.getElementById('level3ID').innerHTML = '<option value="">请选择</option>';
+function get_district(path,d) {
+	var l2=document.getElementById('level2ID');
+	var l3=document.getElementById('level3ID');
+	var va=d.value;
+	if(va=='s1')
+	{
+		var tmp=document.createElement('option');
+		l2.innerHTML='';
+		tmp.value='s2';tmp.innerHTML='请选择';
+		l2.appendChild(tmp);
+		l3.innerHTML='';
+		tmp.value='';
+		l3.appendChild(tmp);
 		return false;
-	} else if (va == 's2') {
-		document.getElementById('level3ID').innerHTML = '<option value="">请选择</option>';
+	}
+	else if(va=='s2')
+	{
+		var tmp=document.createElement('option');
+		tmp.value='';tmp.innerHTML='请选择';
+		l3.innerHTML='';
+		l3.appendChild(tmp);
 		return false;
-	} else {
-		Ajax({
-			url : path + '/address/get_next_level?ID=' + va,
-			onSuccess : function(e) {
-				var dis = JSON.parse(e);
-				var l = dis.length;
-				var n = d.nextSibling.nextSibling;
-				if (d.id == 'level1ID') {
-					n.innerHTML = '<option value="-2">请选择</option>';
-				} else
-					n.innerHTML = '<option value="">请选择</option>';
-				var t = document.createElement('select');
-				for ( var g = 0; g < l; ++g) {
-					var temp = document.createElement('option');
-					temp.value = dis[g].ID;
-					temp.innerHTML = dis[g].name;
+	}
+	else{
+	Ajax({
+		url:path + '/admin/order/get_next_level?ID='+va,
+		onSuccess:function(e)
+			{
+				var dis=JSON.parse(e);
+				var l=dis.length;
+				var n=d.nextSibling.nextSibling;
+				if(d.id=='level1ID')
+				{
+					n.innerHTML='<option value="-2">请选择</option>';
+				}else n.innerHTML='<option value="">请选择</option>';
+				var t=document.createElement('select');
+				for(var g=0;g<l;++g)
+				{
+					var temp=document.createElement('option');
+					temp.value=dis[g].ID;
+					temp.innerHTML=dis[g].name;
 					n.appendChild(temp);
 				}
 			}
