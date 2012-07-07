44a1547bbfd19afbd37d55fed6a70d88b2514345
wanran <shan0xiao0xi@163.com>
2012-07-07 22:03:38 +0800

    修复评论

--- Symagic/org/symagic/common/db/junit/DaoCommentTest.java
+++ Symagic/org/symagic/common/db/junit/DaoCommentTest.java
@@ -1,44 +1,57 @@
 package org.symagic.common.db.junit;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.symagic.common.db.bean.BeanComment;
 import org.symagic.common.db.func.DaoComment;
+import org.symagic.common.db.pool.ConnectionPool;
 
 public class DaoCommentTest {
 	
 	//初始化
-	DaoComment dc = new DaoComment();
 	
-    /*@Test
-	public void testGetAllComment1() {
-        assertEquals(3,dc.getAllComment(1, 3).size());		
-	}
-	@Test
-	public void testGetAllComment2() {
-        assertEquals(0,dc.getAllComment(1, 0).size());		
-	}
-
-	@Test
-	public void testGetAverageRating1() {
-        assertEquals(-1,dc.getAverageRating(1000));
-	}
-	@Test
-	public void testGetAverageRating2() {
-        assertEquals(1,dc.getAverageRating(11));
-	}
 	
-	@Test
-	public void testGetComments(){
-		assertEquals(3,dc.getComment(11, 1, 3).size());
-	}
-	@Test
-	public void testGetComments2(){
-		assertEquals(0,dc.getComment(11, 1, 0).size());
-	}
-	@Test
-	public void testGetComments3(){
-		assertEquals(0,dc.getComment(30, 1, 3).size());
-	}*/
+//    @Test
+//	public void testGetAllComment1() {
+//        assertEquals(3,dc.getAllComment(1, 3).size());		
+//	}
+//	@Test
+//	public void testGetAllComment2() {
+//        assertEquals(0,dc.getAllComment(1, 0).size());		
+//	}
+//
+//	@Test
+//	public void testGetAverageRating1() {
+//        assertEquals(-1,dc.getAverageRating(1000));
+//	}
+//	@Test
+//	public void testGetAverageRating2() {
+//        assertEquals(1,dc.getAverageRating(11));
+//	}
+//	
+//	@Test
+//	public void testGetComments(){
+//		assertEquals(3,dc.getComment(11, 1, 3).size());
+//	}
+//	@Test
+//	public void testGetComments2(){
+//		assertEquals(0,dc.getComment(11, 1, 0).size());
+//	}
+//	@Test
+//	public void testGetComments3(){
+//		assertEquals(0,dc.getComment(30, 1, 3).size());
+//	}
+	
+//	@Test
+//	public void testPublishComment()
+//	{
+//		ConnectionPool.init();
+//		DaoComment dc = new DaoComment();
+//		BeanComment	comment	= new BeanComment();
+//		comment.setBookID(1);
+//		comment.setContent("dfd");
+//		comment.setRating("4");
+//		comment.setUsername("641567179@qq.com");
+//		dc.publishComment(comment);
+//	}
 
 }
